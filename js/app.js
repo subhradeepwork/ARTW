c65817fa7db8c952387cdf05888b6ce33cdc0e37
subhradeepc <subhradeep@geogram.co>
2024-06-05 11:12:09 -0400

    2nd commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/QR.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/QR2.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;

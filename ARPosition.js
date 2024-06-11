078b81d6ab4e137ce6bbb7a120b88167159d5e6d
subhradeepc <subhradeep@geogram.co>
2024-06-10 23:36:54 -0400

    updates

--- ARPosition.js
+++ ARPosition.js
@@ -3,16 +3,21 @@ var unityInstance;
 
 function setUnityInstance(instance) {
     unityInstance = instance;
+    console.log("Unity instance set:", unityInstance);
 }
 
 window.addEventListener("message", function (event) {
+    console.log("Received message:", event.data);
     if (event.data.type === "SetARPosition") {
         setARPosition(event.data.position, event.data.rotation);
     }
 });
 
 function setARPosition(position, rotation) {
+    console.log("Setting AR Position:", position, rotation);
     if (unityInstance != null) {
         unityInstance.SendMessage('MarkerManager', 'OnSetARPosition', JSON.stringify({ position: position, rotation: rotation }));
+    } else {
+        console.log("Unity instance is null.");
     }
 }
